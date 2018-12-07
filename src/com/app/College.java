73d08e1d2050c3a67a9205da57a64fc49883b747
Geetashree <Geetashree@192.168.0.15>
2018-12-07 21:31:58 +0530

    cllg-1.0

---
+++ src/com/app/College.java
@@ -0,0 +1,22 @@
+package com.app;
+
+public class College {
+ private int cllgId;
+ private String cllgName;
+public College() {
+	super();
+}
+public int getCllgId() {
+	return cllgId;
+}
+public void setCllgId(int cllgId) {
+	this.cllgId = cllgId;
+}
+public String getCllgName() {
+	return cllgName;
+}
+public void setCllgName(String cllgName) {
+	this.cllgName = cllgName;
+}
+ 
+}
